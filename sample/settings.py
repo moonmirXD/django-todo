3314a64f0d911ac3b3ff59f5cf905c406b543953
Ryan Arqueza <arquezayan@yahoo.com>
2020-09-05 02:37:24 +0800

    first commit

--- sample/settings.py
+++ sample/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,6 +51,7 @@ REST_FRAMEWORK = {
     ]
 }
 CORS_ALLOW_ALL_ORIGINS = True
+
 
 MIDDLEWARE = [
         'corsheaders.middleware.CorsMiddleware',
@@ -132,3 +134,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+django_heroku.settings(locals())
